c8adb14a8a5a73b6519dd4d84e131bcc9e4bb7a5
Jeffrey <jeffreysebastian@pursuit.org>
2022-04-03 21:44:30 -0400

    heroku fixed end date seed

--- db/seed.sql
+++ db/seed.sql
@@ -8,7 +8,7 @@ INSERT INTO meetingRoom(name, capacity, floor) VALUES
 
 
 INSERT INTO bookings(start_date, end_date, meeting_name, attendees, meetingroom_id)VALUES
-('3/27/2022 11:30', '3/27/2021 12:30','Scrum Standup','jdoe@email.com, bdylan@email.com', 1),
-('4/1/2022 10:30', '4/1/2021 12:30','Scrum Standup','bdylan@email.com, bdylan@email.com', 2),
-('3/28/2022 11:30', '3/28/2021 12:30','Scrum Standup','bdylan@email.com, bdylan@email.com', 1),
-('3/29/2022 11:30', '3/29/2021 12:30','Scrum Standup','jdoe@email.com', 3);
+('3/27/2022 11:30', '3/27/2022 12:30','Scrum Standup','jdoe@email.com, bdylan@email.com', 1),
+('4/1/2022 10:30', '4/1/2022 12:30','Scrum Standup','bdylan@email.com, bdylan@email.com', 2),
+('3/28/2022 11:30', '3/28/2022 12:30','Scrum Standup','bdylan@email.com, bdylan@email.com', 1),
+('3/29/2022 11:30', '3/29/2022 12:30','Scrum Standup','jdoe@email.com', 3);
